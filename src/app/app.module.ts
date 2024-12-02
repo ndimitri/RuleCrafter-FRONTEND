a5339c26d0a13f20a90dcbace24eff6869dafb9a
Krasnikov Viktor <viktor.krasnikov123@gmail.com>
2024-12-02 15:53:40 +0100

    Added Nav Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  // Necessary for animations
+import { SidebarModule } from 'primeng/sidebar';      // Sidebar module for the p-sidebar component
+import { ButtonModule } from 'primeng/button';        // Button module for the pButton component
 import { AppComponent } from './app.component';
+import {NavComponent} from './layout/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,   // For animations (important for PrimeNG components like p-sidebar)
+    SidebarModule,             // For the sidebar component
+    ButtonModule               // For the button component
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/nav/nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+  visibleSidebar: boolean = true;  // Set to true to always show the sidebar
+}
